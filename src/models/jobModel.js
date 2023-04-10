8b0e54d7385e7b49a6b85806ff5cae849534bb40
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-04-10 10:36:20 +0530

    add schemas

--- src/models/jobModel.js
+++ src/models/jobModel.js
@@ -1,18 +1,22 @@
-// models/job.js
-
 const mongoose = require('mongoose');
 
-const jobSchema = new mongoose.Schema(
-    {
-        title: { type: String, required: true },
-        description: { type: String, required: true },
-        company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
-        location: { type: String, required: true },
-        salary: { type: Number, required: true },
-    },
-    { timestamps: true }
-);
+const jobSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  company: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Company', 
+    required: true 
+  },
+  skillsRequired: { 
+    type: [String], 
+    required: true, 
+    enum: ['JavaScript', 'React', 'Node.js', 'HTML', 'CSS', 'MongoDB', 'SQL', 'AWS'] // Limit skills to a predefined list
+  },
+  experienceRequired: { type: Number, required: true, min: 0 },
+  location: { type: String, required: true },
+  salary: { type: Number, required: true },
+  tags:[ { type: String, required: true }],
+});
 
-const Job = mongoose.model('Job', jobSchema);
-
-module.exports = Job;
+module.exports = mongoose.model('Job', jobSchema);
