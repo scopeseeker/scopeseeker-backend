42190fc5683b14b248f53bb5ff6a5e6878fd7fa1
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-05-18 17:12:52 +0530

    directory updated

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,14 @@
-// routes/userRoutes.js
+// // routes/userRoutes.js
 
-const express = require('express');
-// const userController = require('../controllers/userController');
+// const express = require('express');
+// // const userController = require('../controllers/userController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+// // router.get('/', userController.getAllUsers);
+// // router.get('/:id', userController.getUserById);
+// // router.post('/', userController.createUser);
+// // router.put('/:id', userController.updateUser);
+// // router.delete('/:id', userController.deleteUser);
 
-module.exports = router;
+// module.exports = router;
