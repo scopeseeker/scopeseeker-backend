599dfb789e076fcb8874eab7aeb0c2da3496f10b
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-05-26 10:27:39 +0530

    setup: add morgan, helmet and errorHanlder file

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,21 @@
-import { DEBUG_MODE } from "../config";
-import { ValidationError } from 'joi'
+import { DEBUG_MODE } from '../config';
+import { ValidationError } from 'joi';
+
 const errorHandler = (err, req, res, next) => {
-  let statusCode = 500;
-  let data = {
-    message: 'Internal Server Error',
-    ...(DEBUG_MODE === 'true' && {originalMessage: err.message})
-  }
-  if(err instanceof ValidationError){
+    let statusCode = 500;
+    let data = {
+        message: 'Internal server error',
+        ...(DEBUG_MODE === 'true' && { originalError: err.message })
+    }
+
+    if (err instanceof ValidationError) {
         statusCode = 422;
         data = {
             message: err.message
-         }
-  }
-  return res.status().json(data);
-};
+        }
+    }
+
+    return res.status(statusCode).json(data);
+}
+
+export default errorHandler;
